0b4d819f72a43a7d32d32b2112a273a9f51b79bc
LuisMiguelUrbina <urbinagarcialm@outlook.com>
2022-11-26 23:01:44 -0500

    creation of the 'skills' model

--- applications/employees/models.py
+++ applications/employees/models.py
@@ -2,8 +2,24 @@ from django.db import models
 # Related model import
 from applications.departments.models import Department
 
+
+
+# Model for table 'Skills'
+class Skills(models.Model):
+    skill =  models.CharField("Habilidad", max_length = 50)
+
+    # Adaptation to show in the administrator panel
+    class Meta:
+        verbose_name = 'Skill'
+        verbose_name_plural = "Skills"
+
+    def __str__(self):
+         return '(' + str(self.id) + ') ' + self.skill
+
+
 # Model for table 'Employees'
 class Employee(models.Model):
+
     # Choice
     JOB_CHOICES = (
         ('O', 'Generente'),
@@ -15,9 +31,17 @@ class Employee(models.Model):
 
     first_name = models.CharField("Nombre", max_length = 50)
     last_name  = models.CharField("Apellido", max_length = 50)
+    # avatar    = models.ImageField("Imagen", upload_to = None, height_field = None, width_field = None) 
     job        = models.CharField("Cargo", max_length  = 1, choices = JOB_CHOICES)
+    
+    # Foreign keys
     department = models.ForeignKey(Department, on_delete = models.CASCADE)
-    # image      = models.ImageField("Imagen", upload_to = None, height_field = None, width_field = None) 
+    skills     =  models.ManyToManyField(Skills)
+    
+    # Adaptation to show in the administrator panel
+    class Meta:
+        verbose_name = 'Empleado'
+        verbose_name_plural = "Empleados"
 
     def __str__(self):
          return '(' + str(self.id) + ') ' + self.first_name + " " +self.last_name
