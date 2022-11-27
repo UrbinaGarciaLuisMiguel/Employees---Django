64378f14f4a223b903718f030b95fbfde426d1f3
LuisMiguelUrbina <urbinagarcialm@outlook.com>
2022-11-26 23:03:17 -0500

    admin panel customization

--- applications/departments/models.py
+++ applications/departments/models.py
@@ -8,6 +8,11 @@ class Department(models.Model):
     short_name  = models.CharField("Nombre Corto", max_length=20, unique=True)
     is_inactivo = models.BooleanField("Inactivo", default = False)
 
+    # Adaptation to show in the administrator panel
+    class Meta:
+        verbose_name = 'Departamento'
+        verbose_name_plural = "Departamentos"
+
     def __str__(self):
         return '(' + str(self.id) + ')   ' + self.name
     
--- applications/employees/admin.py
+++ applications/employees/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, Skills
 
 # 'Employee' model registration in django admin panel
-admin.site.register(Employee)
+
+admin.site.register(Skills)
+
+
+class EmployeAdmin(admin.ModelAdmin):
+    # Customize field to display in admin panel
+    list_display  = ('id','first_name','last_name','department','job','full_name',)
+    # A browser for the model
+    search_fields = ('first_name',)
+    # Filter parameters
+    list_filter   = ('job', 'skills',) 
+    # Filter for 'skills' (works on many-to-many relationships)
+    filter_horizontal = ('skills',)
+
+    # Extra column in administrator panel that is not in the 'Employees' model defined
+    def full_name(self, obj):
+        return obj.first_name + ' ' + obj.last_name
+
+admin.site.register(Employee, EmployeAdmin)
